3a7332bf97843171f7fad1a55f3fecaa11d91f24
Casey Eastman <datas@Casey-Eastmans-Mac-Pro-2.local>
2013-03-07 02:18:42 -0600

    Initial build

---
+++ TuDeeSkrolla/TuDeeSkrolla/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TuDeeSkrolla
+//
+//  Created by Casey Eastman on 3/7/13.
+//  Copyright (c) 2013 Casey Eastman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TuDeeSkrolla/TuDeeSkrolla/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TuDeeSkrolla
+//
+//  Created by Casey Eastman on 3/7/13.
+//  Copyright (c) 2013 Casey Eastman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
